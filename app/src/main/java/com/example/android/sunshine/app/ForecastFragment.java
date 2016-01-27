1195c2aafdb40dcfcdbfd44b7d893841fccf0a51
David Grandes <david.grandes@olx.com>
2016-01-27 17:40:56 -0300

    Incomplete!!!
    Stopped at Section 4C, Implementing Cursor Loaders.
    
    Eventually finish it please!

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,6 +2,7 @@ package com.example.android.sunshine.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.media.audiofx.BassBoost;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -33,13 +34,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.content.CursorLoader;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements android.app.LoaderManager.LoaderCallbacks<Cursor>{
 
     public String LOG_TAG = this.getClass().getSimpleName();
+
+    public static final int CURSOR_LOADER_ID = 1;
 
     public ArrayAdapter<String> weatherAdapter;
 
@@ -93,6 +100,9 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         refreshData();
+
+        LoaderManager manager = getLoaderManager();
+
         weatherAdapter = new ArrayAdapter<String>(getActivity(),
                 R.layout.list_item_forecast,R.id.list_item_forecast_textview, new ArrayList<String>());
 
@@ -114,128 +124,18 @@ public class ForecastFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public android.content.Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String locationSetting = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("location", null);
+    }
 
-//    private  class FetchWeatherTask extends AsyncTask<Void, Void, String[]> {
-//        int cityId;
-//        int days;
-//        String measurement_system;
-//        public String result;
-//
-//        @Override
-//        protected void onPostExecute(String[] strings) {
-//            super.onPostExecute(strings);
-//            weatherAdapter.clear();
-//
-//            if(strings.length == 0){
-//                Toast toast = Toast.makeText(getActivity(),"Oops!, Couldn't access the API", Toast.LENGTH_LONG);
-//                toast.show();
-//                return;
-//            }
-//
-//            for(String s :strings){
-//                if (s != null ){
-//                    weatherAdapter.add(s);
-//                }else{
-//                    Log.d(LOG_TAG, "null weather string!");
-//                }
-//
-//            }
-//        }
-//
-//        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-//
-//        FetchWeatherTask(int cityId, String measurementSystem, int days){
-//            super();
-//            this.cityId = cityId;
-//            this.measurement_system = measurementSystem;
-//            this.days = days;
-//        }
-//
-//        private String forecastJsonString(int cityId, int days){
-//
-//            HttpURLConnection urlConnection = null;
-//            BufferedReader reader = null;
-//            //final String WEATHER_API = "dc8b5e3bab874dc0c5e45687949095ef";
-//            final String WEATHER_API = "cc19be9fbc52d5e5d92f311f19c6a74e";
-//            try{
-//                Uri.Builder weatherUrl = new Uri.Builder();
-//                weatherUrl.scheme("http");
-//                weatherUrl.authority("api.openweathermap.org");
-//                weatherUrl.path("/data/2.5/forecast/daily");
-//                weatherUrl.appendQueryParameter("id", ((Integer) cityId).toString());
-//                weatherUrl.appendQueryParameter("appId",WEATHER_API);
-//                weatherUrl.appendQueryParameter("cnt", ((Integer)days).toString());
-//                weatherUrl.appendQueryParameter("units", measurement_system);
-//                URL url = new URL(weatherUrl.toString());
-//
-//                // Create the request to OpenWeatherMap, and open the connection
-//                urlConnection = (HttpURLConnection) url.openConnection();
-//                urlConnection.setRequestMethod("GET");
-//                urlConnection.connect();
-//                InputStream inputStream = null;
-//                try{
-//                    inputStream = urlConnection.getInputStream();
-//                }catch(java.io.FileNotFoundException e){
-//                    Log.e(LOG_TAG, "HTTP Endpoint not found!", e);
-//                    return null;
-//                }
-//
-//                StringBuffer buffer = new StringBuffer();
-//                if (inputStream == null){
-//                    Log.e(LOG_TAG,"Input Stream was null");
-//                    return null;
-//                }
-//
-//                reader = new BufferedReader(new InputStreamReader(inputStream));
-//
-//                String line;
-//
-//                while((line = reader.readLine()) != null){
-//                    buffer.append(line + "\n");
-//                }
-//                if (buffer.length() == 0){
-//                    return null;
-//                }
-//                String result = buffer.toString();
-//                return result;
-//
-//            } catch (java.net.ProtocolException e){
-//                Log.e(LOG_TAG, "Protocol Error ", e);
-//                // If the code didn't successfully get the weather data, there's no point in attemping
-//                // to parse it.
-//                return null;
-//            } catch (java.io.IOException e){
-//                Log.e(LOG_TAG, "IO Error ", e);
-//                // If the code didn't successfully get the weather data, there's no point in attemping
-//                // to parse it.
-//                return null;
-//            } finally {
-//                if (urlConnection != null){
-//                    urlConnection.disconnect();
-//                }
-//                if(reader != null){
-//                    try{
-//                        reader.close();
-//                    }catch(final IOException e){
-//                        Log.e("Placeholder Fragment", "Error closing stream", e);
-//                    }
-//                }
-//            }
-//        }
-//
-//        @Override
-//        protected String[] doInBackground(Void... voids) {
-//            String weatherJson = forecastJsonString(this.cityId, this.days);
-//            WeatherDataParser parser = new WeatherDataParser();
-//            try {
-//                String[] forecasts = parser.getWeatherDataFromJson(weatherJson, days);
-//                return forecasts;
-//            } catch (JSONException e) {
-//                e.printStackTrace();
-//                return null;
-//            }
-//        }
-//
-//
-//    }
+    @Override
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+
+    }
 }
